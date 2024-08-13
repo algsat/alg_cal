489663decff2a92ee39cb91d579707bc0bfbc2e1
Hundt, Stephen Adam <shundt@usgs.gov>
2024-08-13 11:27:57 -0600

    refactored script and reorganized into yearly folders

--- flyover_calendar.py
+++ flyover_calendar.py
@@ -1,17 +1,21 @@
 #%%
 # IMPORT
+print('Importing packages...')
 import os
 import numpy as np
 import pandas as pd
 import geopandas as gpd
+import seaborn as sns
+from datetime import datetime
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 from matplotlib.colors import ListedColormap
 import matplotlib as mpl
-import seaborn as sns
-from datetime import datetime
+from matplotlib.lines import Line2D
+from matplotlib import gridspec
 
 # INPUT
+print('Reading inputs and functions...')
 # files
 s2_acq_dates_file = 'data/s2_acqday_dates.csv'
 l8_9_acq_dates_file = 'data/l8_9_acqday_dates.csv'
@@ -27,7 +31,7 @@ ann_graph = True
 mon_graph = True
 path_month_and_ann_calendars = True
 combined_monthly_calendars = True
-years = [2024]
+years = [2024, 2025, 2026]
 colors = [
     '#ffffff', '#663399', '#003366', '#006400', '#ffcccb', '#ffae42'
     , '#98fb98', '#ffffbf', '#d9d9d9', '#d3bda6', '#add8e6', '#ddd3ee'
@@ -98,14 +102,9 @@ def split_months(df, year):
 
     return day_nums, day_vals
 
-# cmap1 = ListedColormap(['#ffffff', '#663399', '#003366', '#006400', '#60ff0000', '#60ff6500', '#60a5d610', '#60ffff00', '#60909090', '#90654321'], name='custom1')
 
 # Taken from SO_tourist answer at: https://stackoverflow.com/questions/32485907/matplotlib-and-numpy-create-a-calendar-heatmap
-
-from matplotlib.lines import Line2D
-from matplotlib import gridspec
-def create_year_calendar(day_nums, day_vals, file_prefix='example', color_dict=None, full_paths=None, part_paths=None, leg_title='track'):
-    # fig, axes = plt.subplots(5, 3, figsize=(6.5, 9))
+def create_year_calendar(day_nums, day_vals, filename, color_dict=None, full_paths=None, part_paths=None, leg_title='track'):
     fig = plt.figure(figsize=(6.5, 9))
     spec = gridspec.GridSpec(ncols=3, nrows=5, height_ratios=[2, 3, 3, 3, 3])
     axes = [fig.add_subplot(spec[i]) for i in range(3, 15)]
@@ -141,20 +140,13 @@ def create_year_calendar(day_nums, day_vals, file_prefix='example', color_dict=N
         else:
             un_vals = np.array([val for val in np.sort(np.unique(day_vals[i+1])) if not np.isnan(val)])
             color_list = [color_dict[val] for val in un_vals]
-            # cmap = sns.color_palette(colors, n_colors=len(colors))
-            # cmap = mpl.colors.Colormap(colors)
             cmap = ListedColormap(color_list)
             norm = mpl.colors.BoundaryNorm([-0.5] + list(un_vals + 0.5) ,cmap.N)
-            # print(cmap)
-            # axs.imshow(day_vals[i+1], interpolation='nearest', origin='lower', cmap=cmap, norm=norm)
             axs.imshow(day_vals[i+1], cmap=cmap, norm=norm)
-        # axs.imshow(day_vals[i+1], cmap=cmap, vmin=-0.05, vmax=1.25)  # heatmap
-        # axs.set_title(month_names[i], fontsize=20)
         axs.set_title(month_names[i], fontsize=10)
 
         # Labels
         axs.set_xticks(np.arange(len(days)))
-        # axs.set_xticklabels(days, fontsize=14, fontweight='bold', color='#555555')
         axs.set_xticklabels(days, fontsize=8, fontweight='bold', color='#555555')
         axs.set_yticklabels([])
 
@@ -178,10 +170,8 @@ def create_year_calendar(day_nums, day_vals, file_prefix='example', color_dict=N
                 day_num = day_nums[i+1][w, d]
                 # If day number is a valid calendar day, add an annotation
                 if not np.isnan(day_num):
-                    # axs.text(d+0.45, w-0.31, f"{day_num:0.0f}",
                     axs.text(d+0.42, w-0.27, f"{day_num:0.0f}",
                              ha="right", va="center",
-                             # fontsize=14, color="#003333", alpha=0.8)  # day
                              fontsize=6, color="#003333", alpha=0.8)  # day
 
                 # Aesthetic background for calendar day number
@@ -196,11 +186,10 @@ def create_year_calendar(day_nums, day_vals, file_prefix='example', color_dict=N
     plt.subplots_adjust(left=0.01, right=0.99, top=0.99, bottom=0.01, wspace=0.01, hspace=0.01)
     fig.tight_layout()
     # Save to file
-    outname = f'images/{file_prefix}.pdf'
-    plt.savefig(outname, dpi=120)
+    plt.savefig(filename, dpi=120)
     plt.close('all')
 
-def create_month_calendars(day_nums, day_vals, file_prefix='example'):
+def create_month_calendars(day_nums, day_vals, filename_base):
     for i in range(0, 12):
         fig, axs = plt.subplots(1, 1, figsize=(5, 4))
         # axs.imshow(day_vals[i+1], cmap='Purples', vmin=-0.05, vmax=1.25)  # heatmap
@@ -251,7 +240,7 @@ def create_month_calendars(day_nums, day_vals, file_prefix='example'):
         fig.tight_layout()
 
         # Save to file
-        outname = f'images/{file_prefix}{i+1:02}.png'
+        outname = filename_base.replace('.png', f'{i+1:02}.png')
         plt.savefig(outname, dpi=120)
         plt.close('all')
 
@@ -261,7 +250,7 @@ def first_pass_of_year(start_date, year, interval):
     first_dt = year_start_dt + pd.to_timedelta(offset, 'D')
     return first_dt.strftime('%Y-%m-%d')
 
-def ann_calendar_text(acqs, full_paths, part_paths, yr_starts, basename, pass_int):
+def ann_calendar_text(acqs, full_paths, part_paths, yr_starts, pass_int):
     full_acqs = [int(x) for x in acqs if 'p' not in x]
     part_acqs = [int(x[:-1]) for x in acqs if 'p' in x]
     full_start_dates = yr_starts.query(f'acqday in {full_acqs}').start_date.values.tolist()
@@ -285,15 +274,10 @@ def ann_calendar_text(acqs, full_paths, part_paths, yr_starts, basename, pass_in
         .rename(columns={'path': colname})
         .sort_values(['date'])
     )
-    (
-        df_all
-        .drop(['colorid'], axis=1)
-        .to_csv(f'text_calendars/{basename}.csv', index=False)
-    )
     return df_all
 
 # make graphical calendars
-def ann_calendar_graph(df_all, yr_dates, basename, color_dict, full_paths, part_paths):
+def ann_calendar_graph(df_all, yr_dates, filename, color_dict, full_paths, part_paths):
     date_gen = pd.DataFrame({
         'date': yr_dates
         , 'colorid':0
@@ -307,9 +291,9 @@ def ann_calendar_graph(df_all, yr_dates, basename, color_dict, full_paths, part_
     )
     df_dates = pd.Series(color_ids.values, index=yr_dates)
     day_nums, day_vals = split_months(df_dates, year)
-    create_year_calendar(day_nums, day_vals, file_prefix=basename, color_dict=color_dict, full_paths=full_paths, part_paths=part_paths)
+    create_year_calendar(day_nums, day_vals, filename, color_dict=color_dict, full_paths=full_paths, part_paths=part_paths)
 
-def month_calendar_graph(df_all, yr_dates, basename, color_dict):
+def month_calendar_graph(df_all, yr_dates, filename_base, color_dict):
     date_gen = pd.DataFrame({
         'date': yr_dates
         , 'colorid':0
@@ -323,7 +307,7 @@ def month_calendar_graph(df_all, yr_dates, basename, color_dict):
     )
     df_dates = pd.Series(color_ids.values, index=yr_dates)
     day_nums, day_vals = split_months(df_dates, year)
-    create_month_calendars(day_nums, day_vals, file_prefix=basename)
+    create_month_calendars(day_nums, day_vals, filename_base)
 
 def make_month_calendar(ax, day_nums, day_vals, color_dict, mon_i):
     # get cmap
@@ -367,7 +351,7 @@ def make_month_calendar(ax, day_nums, day_vals, color_dict, mon_i):
             ax.add_artist(triangle)
     return ax
 
-def create_comb_month_calendars(s2_day_nums, l8_9_day_nums, s2_day_vals, l8_9_day_vals, color_dict, file_prefix='example'):
+def create_comb_month_calendars(s2_day_nums, l8_9_day_nums, s2_day_vals, l8_9_day_vals, color_dict, filename_base):
     for i in range(0, 12):
         # set up grid
         fig = plt.figure(figsize=(10, 5))
@@ -398,11 +382,11 @@ def create_comb_month_calendars(s2_day_nums, l8_9_day_nums, s2_day_vals, l8_9_da
         # plt.subplots_adjust(left=0.04, right=0.96, top=0.96, bottom=0.04)
         # fig.tight_layout()
         # Save to file
-        outname = f'images/{file_prefix}{i+1:02}.png'
+        outname = filename_base.replace('png', f'{i:02}.png')
         plt.savefig(outname, dpi=120)
         plt.close('all')
 
-def month_comb_calendar_graph(s2_df_all, l8_9_df_all, yr_dates, basename, color_dict):
+def month_comb_calendar_graph(s2_df_all, l8_9_df_all, yr_dates, filename_base, color_dict):
     date_gen = pd.DataFrame({
         'date': yr_dates
         , 'colorid':0
@@ -425,11 +409,21 @@ def month_comb_calendar_graph(s2_df_all, l8_9_df_all, yr_dates, basename, color_
     )
     df_l8_9_dates = pd.Series(l8_9_color_ids.values, index=yr_dates)
     l8_9_day_nums, l8_9_day_vals = split_months(df_l8_9_dates, year)
-    create_comb_month_calendars(s2_day_nums, l8_9_day_nums, s2_day_vals, l8_9_day_vals, color_dict, file_prefix=basename)
+    create_comb_month_calendars(s2_day_nums, l8_9_day_nums, s2_day_vals, l8_9_day_vals, color_dict, filename_base)
 
 # BODY
 # def main():
 if 1:
+    print('Loading files and defining paths and acquisition schedules...')
+    # define some settings
+    intervals = {'s2': 5, 'l8_9': 8}
+    col_names = {'s2': 'track', 'l8_9': 'path'}
+    weeks = [1, 2, 3, 4, 5, 6]
+    days = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
+    month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+                   'September', 'October', 'November', 'December']
+    color_dict = {i: col for i,col in enumerate(colors)}
+    dateparser = lambda x: datetime.strptime(x, "%Y-%m-%d")
     # load files
     wb_coverage = pd.read_csv(wb_coverage_file, dtype={x: 'str' for x in ['s2_full', 's2_part', 's2_id', 's2_acq_id', 'l8_9_full', 'l8_9_part', 'l8_9_id', 'l8_9_acq_id']})
     l8_9_dates = pd.read_csv(l8_9_acq_dates_file)
@@ -472,98 +466,92 @@ if 1:
     )
     s2_overlap_acq = [str(sorted(set([int(s2_track_acq_dict[int(id)]) for id in ids.split('_')])))[1: -1].replace(', ', '_') for ids in s2_overlap]
     s2_overlap_ids = pd.DataFrame({'s2_id': s2_overlap, 's2_acq_id': s2_overlap_acq})
-    # todo: here - make id files
     # define some settings
-    intervals = {'s2': 5, 'l8_9': 8}
     start_dates = {'s2': s2_dates, 'l8_9': l8_9_dates}
     cov_ids = {'s2': s2_ids, 'l8_9': l8_9_ids}
     over_cov_ids = {'s2': s2_overlap_ids, 'l8_9': l8_9_overlap_ids}
-    col_names = {'s2': 'track', 'l8_9': 'path'}
-    weeks = [1, 2, 3, 4, 5, 6]
-    # days = ['M', 'T', 'W', 'T', 'F', 'S', 'S']
-    # todo: switch to sunday as beginning of week???
-    days = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
-    month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
-                   'September', 'October', 'November', 'December']
-    color_dict = {i: col for i,col in enumerate(colors)}
-    dateparser = lambda x: datetime.strptime(x, "%Y-%m-%d")
+    # counts of each made
+    text_cal_num = 0
+    ann_cal_num = 0
+    mon_cal_num = 0
+    comb_cal_num = 0
     # loop over satellites
+    print('Generating output by satellite...')
     for sat in ['s2', 'l8_9']:
+        print('Satellite:', sat)
         pass_int = intervals[sat]
         ids = cov_ids[sat]
         over_ids = over_cov_ids[sat]
         colname = col_names[sat]
         # loop over years
         for year in years:
+            print('Year:', year)
             yr_starts = (
                 start_dates[sat]
                 .assign(start_date = lambda x: [first_pass_of_year(date, year, pass_int) for date in x.date])
             )
             yr_dates = pd.date_range(f'{year}-01-01', end=f'{year}-12-31', freq='D')
-            if wb_ann_calendars:
-                # loop over ids
-                for id, acq_id in ids.itertuples(index=False, name=None):
-                    basename = f'{sat}_{id}_{year}'
-                    filename = f'text_calendars/{basename}.csv'
-                    if (not os.path.exists(filename)) or (not os.path.exists(filename.replace('.pdf', '01.png'))):
-                        paths = id.split('_')
-                        full_paths = [int(x) for x in paths if 'p' not in x]
-                        part_paths = [int(x[:-1]) for x in paths if 'p' in x]
-                        acqs = acq_id.split('_')
-                        # make csv of dates
-                        df_all = ann_calendar_text(acqs, full_paths, part_paths, yr_starts, basename, pass_int)
-                    # make graphical calendars
-                    if ann_graph:
-                        if not os.path.exists(filename):
-                            ann_calendar_graph(df_all, yr_dates, basename, color_dict, full_paths, part_paths)
-                            plt.close('all')
-                    if mon_graph:
-                        if not os.path.exists(filename.replace('.pdf', '01.png')):
-                            if len(part_paths)==0:
-                                month_calendar_graph(df_all, yr_dates, basename, color_dict)
-                                plt.close('all')            
-            if path_month_and_ann_calendars:
-                for id, acq_id in over_ids.itertuples(index=False, name=None):
-                    # if id not in list(ids.iloc[:, 0]):
-                    if 1:
-                        basename = f'{sat}_{id}_{year}'
-                        filename = f'images/{basename}.pdf'
-                        if (not os.path.exists(filename)) or (not os.path.exists(filename.replace('.pdf', '01.png'))):
-                            paths = id.split('_')
-                            full_paths = [int(x) for x in paths if 'p' not in x]
-                            part_paths = [int(x[:-1]) for x in paths if 'p' in x]
-                            acqs = acq_id.split('_')
-                            # make csv of dates
-                            df_all = ann_calendar_text(acqs, full_paths, part_paths, yr_starts, basename, pass_int)
-                            # make graphical calendars
-                            if ann_graph:
-                                if not os.path.exists(filename):
-                                    ann_calendar_graph(df_all, yr_dates, basename, color_dict, full_paths, part_paths)
-                                    plt.close('all')
-                            if mon_graph:
-                                if not os.path.exists(filename.replace('.pdf', '01.png')):
-                                    month_calendar_graph(df_all, yr_dates, basename, color_dict)
-                                    plt.close('all')
-    if combined_monthly_calendars:
-        for s2_id, l8_9_id in s2_l8_9_ids:
-            for year in years:
-                basename = f's2_{s2_id}_l8_9_{l8_9_id}_{year}'
-                if not os.path.exists(f'images/{basename}01.png'):
-                    s2_text_name = f'text_calendars/s2_{s2_id}_{year}.csv'
-                    l8_9_text_name = f'text_calendars/l8_9_{l8_9_id}_{year}.csv'
-                    s2_df_all = (
-                        pd
-                        .read_csv(s2_text_name, parse_dates=[0], date_parser=dateparser)
-                        .assign(colorid = lambda x: [1 if cov=='full' else 4 for cov in x.coverage])
-                    )
-                    l8_9_df_all = (
-                        pd
-                        .read_csv(l8_9_text_name, parse_dates=[0], date_parser=dateparser)
-                        .assign(colorid = lambda x: [1 if cov=='full' else 4 for cov in x.coverage])
-                    )
-                    yr_dates = pd.date_range(f'{year}-01-01', end=f'{year}-12-31', freq='D')
-                    month_comb_calendar_graph(s2_df_all, l8_9_df_all, yr_dates, basename, color_dict)
+            # if wb_ann_calendars:
+            ids_all = pd.concat([ids, over_ids]).drop_duplicates()
+            # loop over ids
+            print('Generating output by id...')
+            for id, acq_id in ids.itertuples(index=False, name=None):
+                basename = f'{sat}_{id}_{year}'
+                paths = id.split('_')
+                full_paths = [int(x) for x in paths if 'p' not in x]
+                part_paths = [int(x[:-1]) for x in paths if 'p' in x]
+                acqs = acq_id.split('_')
+                df_all = ann_calendar_text(acqs, full_paths, part_paths, yr_starts, pass_int)
+                 # make csv of dates
+                filename = os.path.join(str(year), 'text_calendars', f'{basename}.csv')
+                if (not os.path.exists(filename)):
+                    df_all.drop(['colorid'], axis=1).to_csv(filename, index=False)
+                    text_cal_num+=1
+                    if text_cal_num % 100 == 0:
+                        print(f'{text_cal_num} text calendars made')
+                # make annual calendars
+                filename = os.path.join(str(year), 'annual_calendars', f'{basename}.pdf')
+                if (not os.path.exists(filename)):
+                    ann_calendar_graph(df_all, yr_dates, filename, color_dict, full_paths, part_paths)
                     plt.close('all')
+                    ann_cal_num+=1
+                    if ann_cal_num % 100 == 0:
+                        print(f'{ann_cal_num} annual calendars made')
+                # make monthly calendars
+                filename = os.path.join(str(year), 'monthly_calendars', f'{basename}12.png')
+                if (not os.path.exists(filename)) and (len(part_paths)==0):
+                    filename = os.path.join(str(year), 'monthly_calendars', f'{basename}.png')
+                    month_calendar_graph(df_all, yr_dates, filename, color_dict)
+                    plt.close('all')            
+                    mon_cal_num+=1
+                    if mon_cal_num % 1200 == 0:
+                        print(f'{mon_cal_num} monthly calendars made')
+    print('Generating figures of combined satellite calendars...')
+    for s2_id, l8_9_id in s2_l8_9_ids:
+        for year in years:
+            print('Year:', year)
+            basename = f's2_{s2_id}_l8_9_{l8_9_id}_{year}'
+            filename = os.path.join(str(year), 'combined_monthly_calendars', f'{basename}12.png')
+            if not os.path.exists(filename):
+                filename = os.path.join(str(year), 'combined_monthly_calendars', f'{basename}.png')
+                s2_text_name = os.path.join(str(year), 'text_calendars', f's2_{s2_id}_{year}.csv')
+                l8_9_text_name = os.path.join(str(year), 'text_calendars', f'l8_9_{l8_9_id}_{year}.csv')
+                s2_df_all = (
+                    pd
+                    .read_csv(s2_text_name, parse_dates=[0], date_parser=dateparser)
+                    .assign(colorid = lambda x: [1 if cov=='full' else 4 for cov in x.coverage])
+                )
+                l8_9_df_all = (
+                    pd
+                    .read_csv(l8_9_text_name, parse_dates=[0], date_parser=dateparser)
+                    .assign(colorid = lambda x: [1 if cov=='full' else 4 for cov in x.coverage])
+                )
+                yr_dates = pd.date_range(f'{year}-01-01', end=f'{year}-12-31', freq='D')
+                month_comb_calendar_graph(s2_df_all, l8_9_df_all, yr_dates, filename, color_dict)
+                plt.close('all')
+                comb_cal_num+=1
+                if comb_cal_num % 1200 == 0:
+                    print(f'{comb_cal_num} combined calendars made')
 # if __name__ == "main":
 #     main(False
 
